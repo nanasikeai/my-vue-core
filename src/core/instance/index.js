1949024ad77177f23bbe83cf7593a7e50dface5a
yangyinghao <yangyinghao4565645@163.com>
2023-05-25 11:29:44 +0800

    删除测试代码

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -3,8 +3,6 @@ import {lifecycleMixin} from "./lifecycle";
 import {renderMixin} from "../../vnode/index";
 import {initGlobalApi} from "../global-api/index";
 import Watcher, {nextTick} from "../observe/watcher";
-import {compileToFunctions} from "../../compiler/index";
-import {createEl, patch} from "../../vnode/patch";
 
 function Vue(options) {
     this._init(options)
@@ -17,34 +15,5 @@ initMixin(Vue)
 lifecycleMixin(Vue) // 添加生命周期
 renderMixin(Vue) // 添加_render方法
 initGlobalApi(Vue)
-
-setTimeout(() =>{
-    let render1 = compileToFunctions(`<ul key="t" style="color: red">
-        <li key="a">a</li>
-        <li key="b">b</li>
-        <li key="c">c</li>
-        <li key="d">d</li>
-    </ul>`)
-    let vm1 = new Vue({data: {name: '杨'}})
-    let preVnode = render1.call(vm1)
-    let el1 = createEl(preVnode)
-    document.body.appendChild(el1)
-
-    let render2 = compileToFunctions(`<ul key="t" style="color: green">   
-        <li key="d">d</li>    
-        <li key="c">c</li>
-        <li key="b">b</li>
-        <li key="a">a</li>
-    </ul>`)
-    let vm2 = new Vue({data: {name: '杨'}})
-    let nextVnode = render2.call(vm2)
-
-// 不直接替换真实节点，而是比较他们之间的差异再生成真实dom
-    setTimeout(() =>{
-        patch(preVnode, nextVnode)
-    },3000)
-},500)
-
-
 
 export default Vue
